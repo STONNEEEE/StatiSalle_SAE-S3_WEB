e7c4cd2280596f174a61b277b9667c7de7c0e174
Robin Montes <robin.montes@iut-rodez.fr>
2024-12-11 09:10:45 +0100

    Correction de nom des fichier css/php

--- pages/affichageSalle.php
+++ pages/affichageSalle.php
@@ -19,58 +19,57 @@
         <div>
             <h1>Liste des Salles</h1>
         </div>
-        <div class="col-12 col-md-4 d-flex justify-content-center justify-content-sm-start mb-3 mb-md-0">
-            <select class="select-filtre mb-3 mb-md-0">
-                <option selected>Nom du filtre</option>
-                <option>Filtre 1</option>
-                <option>Filtre 2</option>
-                <option>Filtre 3</option>
-            </select>
-            <select class="select-filtre mb-3 mb-md-0">
-                <option selected>Nom du filtre</option>
-                <option>Filtre 1</option>
-                <option>Filtre 2</option>
-                <option>Filtre 3</option>
-            </select>
-            <select class="select-filtre mb-3 mb-md-0">
-                <option selected>Nom du filtre</option>
-                <option>Filtre 1</option>
-                <option>Filtre 2</option>
-                <option>Filtre 3</option>
-            </select>
+        <div class="col-12 d-flex justify-content-between align-items-center mb-3">
+            <!-- Filtres alignés à gauche -->
+            <div class="d-flex">
+                <select class="select-filtre me-2">
+                    <option selected>Nom du filtre</option>
+                    <option>Filtre 1</option>
+                    <option>Filtre 2</option>
+                    <option>Filtre 3</option>
+                </select>
+                <select class="select-filtre me-2">
+                    <option selected>Nom du filtre</option>
+                    <option>Filtre 1</option>
+                    <option>Filtre 2</option>
+                    <option>Filtre 3</option>
+                </select>
+                <select class="select-filtre me-2">
+                    <option selected>Nom du filtre</option>
+                    <option>Filtre 1</option>
+                    <option>Filtre 2</option>
+                    <option>Filtre 3</option>
+                </select>
+            </div>
+            <!-- Bouton aligné à droite -->
+            <button class="btn-filtrer"><span class="fa-plus">Ajouter</button>
         </div>
         <div>
-            <table>
-                <table class="table table-striped">
-                    <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
-                    </tbody>
-                </table>
+            <table class="table table-striped">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                </tr>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                </tr>
             </table>
         </div>
         <?php include '../fonction/footer.php'; ?>
