cd9f85a9b2ed7d825540507516a8cbf0aa116a57
V5lMG <114313700+V5lMG@users.noreply.github.com>
2024-12-13 11:45:09 +0100

    Ajout defer

--- pages/accueil.php
+++ pages/accueil.php
@@ -59,5 +59,5 @@
         </div>
     </body>
     <!-- Bootstrap JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
 </html>
