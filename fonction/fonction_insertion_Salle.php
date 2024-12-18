771f3459860da2653d057c2f71b23e04e4afa38b
erwan.thierry <erwan.thierry@iut-rodez.fr>
2024-12-18 14:08:15 +0100

    Page de création de Salle pour administrateur terminé

--- fonction/fonction_insertion_Salle.php
+++ fonction/fonction_insertion_Salle.php
@@ -9,20 +9,19 @@
 
         try {
 
-        $stmt = $pdo->prepare("INSERT INTO salle (Ident, Nom, Capacite, videoproj, ecranXXL, ordinateur, type, logiciels, imprimante) 
-                                   VALUES (:Nom, :Capacite, :videoproj, :ecranXXL, :nbrOrdi, :type, :logiciels, :imprimante)");
+        $stmt = $pdo->prepare("INSERT INTO salle ( nom, capacite, videoproj, ecran_xxl, ordinateur, type, logiciels, imprimante) 
+                                   VALUES (:Nom, :Capacite, :videoproj, :ecranXXL, :nbrOrdi, :type, :logiciels, :imprimante);");
         $stmt->bindParam(':Nom', $nomSalle);
         $stmt->bindParam(':Capacite', $capacite);
         $stmt->bindParam(':videoproj', $videoProjecteur);
         $stmt->bindParam(':ecranXXL', $ordinateurXXL);
         $stmt->bindParam(':nbrOrdi', $nbrOrdi);
-        $stmt->bindParam(':type', $type);
+        $stmt->bindParam(':type', $typeMateriel);
         $stmt->bindParam(':logiciels', $logiciel);
         $stmt->bindParam(':imprimante', $imprimante);
         $stmt->execute();
-        // Recuperer l'ID de le derniere insertion
-        $lastInsertId = $pdo->lastInsertId();
-        echo "<div class='alert alert-success'>Les données ont été insérées avec succès. ID créé : " . $lastInsertId . "</div>";
+
+        echo "<div class='alert alert-success'>Les données ont été insérées avec succès. ID créé : </div>";
     } catch (PDOException $e) {
         echo "<div class='alert alert-danger'>Erreur lors de l'insertion : " . $e->getMessage() . "</div>";
     }
