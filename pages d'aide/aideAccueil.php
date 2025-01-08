2f90a77dc22de83ca8d84a947ef4e68b86cf63d4
Robin Montes <robin.montes@iut-rodez.fr>
2025-01-08 21:56:10 +0100

    Ajout de page d'aide

---
+++ pages d'aide/aideAccueil.php
@@ -0,0 +1,80 @@
+<?php
+require("../fonction/connexion.php");
+session_start();
+verif_session();
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>StatiSalle - aides Accueil</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <!-- CSS -->
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../css/footer.css">
+    <!-- Icon du site -->
+    <link rel="icon" href=" ../img/logo.ico">
+</head>
+<body>
+<div class="container-fluid">
+    <!-- Header de la page -->
+    <?php include '../include/header.php'; ?>
+
+    <div class="full-screen padding-header">
+        <div class="row text-center">
+            <h1>StatiSalle</h1>
+        </div>
+
+        <div class="row d-flex justify-content-center align-items-start w-100 mb-5">
+            <div class="acc-container p-4 w-50">
+                <!-- Champ Identifiant -->
+                <div class="mb-3">
+                    <div class="d-flex flex-column flex-sm-row justify-content-between">
+                        <label for="identifiant" class="form-label mb-1 mb-sm-0">Identifiant</label>
+                        <small class="form-text text-sm-start text-md-end">
+                            <a href="#" target="_blank" class="text-danger text-decoration-none" title="Identifiant oublié">
+                                Identifiant oublié ?
+                            </a>
+                        </small>
+                    </div>
+                    <input type="text" class="form-control mt-2 mt-sm-0" name="identifiant" id="identifiant" placeholder="Entrez votre identifiant">
+                </div>
+                <p>
+                    Dans ce champ, vous devez entrer l’identifiant de votre compte pour pouvoir accéder au reste des pages et fonctionnalités du site. La fonctionnalité de <b>l'identifiant oublié ne fonctionne pas</b>, elle est là pour montrer que cette fonctionnalité pourrait implémenter dans le futur.
+                </p>
+                <!-- Champ mot de passe -->
+                <div class="mb-3">
+                    <!-- Container pour label et small -->
+                    <div class="d-flex flex-column flex-sm-row justify-content-between">
+                        <label for="mdp" class="form-label mb-1 mb-sm-0">Mot de passe</label>
+                        <small class="form-text text-sm-start text-md-end">
+                            <a href="#" target="_blank" class="text-danger text-decoration-none" title="Mot de passe oublié">
+                                Mot de passe oublié ?
+                            </a>
+                        </small>
+                    </div>
+                    <input type="password" class="form-control mt-2 mt-sm-0" name="mdp" id="mdp" placeholder="Entrez votre mot de passe">
+                </div>
+                <p>
+                    Dans ce champ, vous devez entrer le mot de passe concernant l’identifiant entrer au-dessus afin d'accéder au reste du site. La fonctionnalité de <b>mot de passe oublié ne fonctionne pas</b>, elle est là pour montrer que cette fonctionnalité pourrait implémenter dans le futur.
+                    <br>
+                </p>
+                <button type="submit" class="btn btn-info w-100">Se connecter</button>
+                <p>
+                    <br>
+                    Une fois l’identifiant et le mot de passe saisis, il faut cliquer sur ce bouton pour effectuer une tentative de connexion.
+                    Si jamais l’identifiant ou le mot de passe n’est pas juste, un message d’erreur s’affiche.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <!-- Footer de la page -->
+    <?php include '../include/footer.php'; ?>
+</div>
+</body>
+</html>
---
+++ pages d'aide/aideAffichageEmploye.php
@@ -0,0 +1,107 @@
+<?php
+require("../fonction/connexion.php");
+session_start();
+verif_session();
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>StatiSalle - aides exportation</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <!-- CSS -->
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../css/footer.css">
+    <!-- Icon du site -->
+    <link rel="icon" href=" ../img/logo.ico">
+</head>
+<body>
+<div class="container-fluid">
+    <!-- Header de la page -->
+    <?php include '../include/header.php'; ?>
+
+    <div class="full-screen padding-header">
+        <div class="row text-center">
+            <h1>StatiSalle</h1>
+        </div>
+
+        <div class="row d-flex justify-content-center align-items-start w-100 mb-5">
+            <div class="acc-container p-4 w-50">
+                <p>
+                    Tout d’abord pour l’affichage des employés, nous avons le nom, prénom, identifiant et numéro de téléphone de l’employé. Mais nous avons aussi un bouton pour la modification de l’employé qui redirige une nouvelle page contenant un formulaire pour la modification. Pour plus d’informations, vous pouvez aller voir la page d’aide de la modification.
+                    Il y a également un bouton pour supprimer l’utilisateur. Nous avons aussi mis un petit bouclier pour montrer que l’utilisateur est un administrateur.
+                </p>
+                <table class="table table-striped">
+                    <tr>
+                        <th>Nom</th>
+                        <th>Prénom</th>
+                        <th>Identifiant compte</th>
+                        <th>Numéro de téléphone</th>
+                        <th></th>
+                    </tr>
+                    <tr>
+                        <td>User nom</td>
+                        <td>User prénom</td>
+                        <td>user</td>
+                        <td>0000</td>
+                        <td>
+                            <button type="submit" class="btn-suppr rounded-2">
+                                <span class="fa-solid fa-trash"></span>
+                            </button>
+                            <button type="submit" class="btn-modifier rounded-2">
+                                <span class="fa-regular fa-pen-to-square"></span>
+                            </button>
+                        </td>
+                    </tr>
+                </table>
+                <p>
+                    Ensuite, il y a des filtres qui permettent de faciliter la recherche d’un employé avec le nom, le prénom le compte employé qui signifie l’identifiant et le numéro de téléphone.
+                </p>
+                <!-- Champs de filtres -->
+                <div class="row g-1 justify-content-start">
+                    <!-- Nom employé -->
+                    <div class="col-12 col-md-2 mb-1">
+                        <input type="text" class="form-control" id="nom" name="nom" placeholder="Nom">
+                    </div>
+                    <!-- Prénom employé -->
+                    <div class="col-12 col-md-2 mb-1">
+                        <input type="text" class="form-control" id="prenom" name="prenom" placeholder="Prénom">
+                    </div>
+                    <!-- ID compte employé -->
+                    <div class="col-4">
+                        <input type="text" class="form-control" id="compte" name="compte" placeholder="Compte employé">
+                    </div>
+                    <!-- Numéro de téléphone employé -->
+                    <div class="col-4">
+                        <input type="text" class="form-control" id="telephone" name="telephone" placeholder="Numéro de téléphone">
+                    </div>
+                </div>
+                <p>
+                    Pour supprimer le contenu de tous les filtres il vous suffit de cliquer sur le bouton réinitialiser filtre.
+                </p>
+                <!-- Bouton de soumission -->
+                <div class="col-4">
+                    <button class="btn-reset rounded-1 w-100" type="submit">Réinitialiser filtres</button>
+                </div>
+                <p>
+                    Nous avons aussi un bouton ajouter qui va vous rediriger vers une nouvelle page qui contient un formulaire pour créer un nouvel utilisateur dans la base de données du site.
+                    Pour plus d'informations, allez sur la page d’aide de la page de création d’un employé.
+                </p>
+                <div class="col-12">
+                    <button class="btn-bleu rounded-2" type="button";">
+                        <span class="fa-plus"></span> Ajouter
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Footer de la page -->
+    <?php include '../include/footer.php'; ?>
+</div>
+</body>
+</html>
